8d69916f9448ef1b13db2b2e2f4689fe3f6dd0e6
Bruno Silverio <bcamilo94@gmail.com>
2023-11-22 14:01:00 -0300

    feat: add function to validate new order

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -1,5 +1,7 @@
 package entity
 
+import "errors"
+
 type Order struct {
 	ID         string
 	Price      float64
@@ -13,6 +15,19 @@ func NewOrder(id string, price float64, tax float64) *Order {
 		Price: price,
 		Tax:   tax,
 	}
+}
+
+func (o *Order) Validate() error {
+	if o.ID == "" {
+		return errors.New("id is required")
+	}
+	if o.Price <= 0 {
+		return errors.New("price must be greater than zero")
+	}
+	if o.Tax <= 0 {
+		return errors.New("invalid tax")
+	}
+	return nil
 }
 
 func (o *Order) CalculateFinalPrice() {
