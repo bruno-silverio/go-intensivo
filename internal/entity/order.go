a01a11bb2f15830808ab14cad8adcba2d1ddee98
Bruno Silverio <bcamilo94@gmail.com>
2023-11-22 13:56:44 -0300

    feat: add function for new order

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -7,6 +7,14 @@ type Order struct {
 	FinalPrice float64
 }
 
+func NewOrder(id string, price float64, tax float64) *Order {
+	return &Order{
+		ID:    id,
+		Price: price,
+		Tax:   tax,
+	}
+}
+
 func (o *Order) CalculateFinalPrice() {
 	o.FinalPrice = o.Price + o.Tax
 }
